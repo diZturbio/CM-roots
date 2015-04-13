91e2d437da4d7eff0aebc9c21730db7817efd182
diZturbio <atari.disturbio@gmail.com>
2015-04-13 17:55:43 +0100

    mobile version mostly done, news slider still needs work

--- templates/content-fotorama.php
+++ templates/content-fotorama.php
@@ -4,14 +4,16 @@
 	  	<?php 
 			if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
 				$url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>
-				<div class="col-sm-4 entry-image">
+				<div class="col-sm-5 entry-image">
 					<a href="<?php the_permalink(); ?>"><img class="img-responsive" src="<?php echo $url;?>"  /></a>
 				</div>
 		<?php	} 
 		?>
 		<?php // get_template_part('templates/entry-date'); ?>
-	    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-		<div class="entry-summary"><?php the_excerpt(); ?></div>
+		<div class="col-sm-7 entry-text">
+		    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+			<div class="entry-summary hidden-xs"><?php the_excerpt(); ?></div>
+		</div>
 	</div>
 
   <?php // get_template_part('templates/entry-cats'); ?>
